aa7909c390fa4ddd5af14ab0de12a4a6ce2d1c8f
imnixan <vcmvenom@gmail.com>
2023-09-29 22:07:53 +0700

    makeMoveFromSlide

---
+++ Assets/Scripts/LevelsData/LevelData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelData", menuName = "LevelData", order = 0)]
+public class LevelData : ScriptableObject
+{
+    public Sprite[] Hints;
+    public Sprite[] CorrectPieces;
+    public Sprite[] IncorrentPieces;
+}
---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Piece : MonoBehaviour
+{
+    private bool dragged;
+    private Vector2 startPos;
+    private RectTransform rt;
+    private Transform t;
+    private Image image;
+    private Vector2 mousePos;
+    private Camera camera;
+    private float minYPos;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        rt = GetComponent<RectTransform>();
+        camera = Camera.main;
+        t = transform;
+        minYPos = t.position.y + 2;
+    }
+
+    private void OnMouseDown()
+    {
+        startPos = rt.anchoredPosition;
+        dragged = true;
+        image.color = Color.red;
+    }
+
+    private void OnMouseUp()
+    {
+        image.color = Color.white;
+        dragged = false;
+        rt.anchoredPosition = startPos;
+    }
+
+    private void Update()
+    {
+        if (dragged)
+        {
+            mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+            if (mousePos.y >= minYPos)
+            {
+                t.position = mousePos;
+            }
+            else
+            {
+                rt.anchoredPosition = startPos;
+            }
+        }
+    }
+}
